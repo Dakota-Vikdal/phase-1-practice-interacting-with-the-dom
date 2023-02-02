f03db95528dfee5839051fb0e57193cc85418dab
Laurence Fairbanks <laurencefairbanks@yahoo.com>
2023-02-02 09:37:02 -0600

    claimed minus

--- js/challenge.js
+++ js/challenge.js
@@ -1,4 +1,7 @@
 //const variable = [1,2,3];
 
 const plus = document.querySelector("#plus")
-console.log(plus)
+//console.log(plus)
+
+const minus = document.querySelector("#minus")
+console.log(minus)
