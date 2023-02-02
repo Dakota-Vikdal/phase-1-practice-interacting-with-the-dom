1cf927453f3c49182feaebcb975a1fd9a69f2e72
Dakota Vikdal <dakota.james19@hotmail.com>
2023-02-02 10:51:26 -0500

    incrementing button

--- js/challenge.js
+++ js/challenge.js
@@ -4,4 +4,19 @@ const plus = document.querySelector("#plus")
 //console.log(plus)
 
 const minus = document.querySelector("#minus")
-console.log(minus)
+//console.log(minus)
+
+
+plus.addEventListener("click", () => {
+    plusButton();
+})
+
+function plusButton () {
+    const timer = document.querySelector("#counter")
+    parseInt(timer)
+ 
+    if (timer >= 0) {
+        return timer.textContent = counter++;
+    } 
+    
+};
